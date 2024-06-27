40d9035a60a6f29b88c9a1d86415ba8714415249
vaibhav04861 <vaibhavbv1@gmail.com>
2024-06-27 11:43:05 +0530

    sidebar1

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,3 +1,4 @@
+// src/Sidebar.js
 import React, { useState } from 'react';
 import './sidebar.css';
 import { SideBarData } from './SideBarData';
@@ -5,10 +6,19 @@ import PieChartIcon from '@mui/icons-material/PieChart';
 
 export const Sidebar = () => {
   const [showTitles, setShowTitles] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
 
   const handleClick = () => {
     setShowTitles(prevState => !prevState);
   };
+
+  const handleItemClick = (key) => {
+    setSelectedItem(prevSelectedItem => (prevSelectedItem === key ? null : key));
+  };
+
+  // Separate the main items from the bottom items
+  const mainItems = SideBarData.filter(item => item.title !== 'Gift Card' && item.title !== 'Log Out');
+  const bottomItems = SideBarData.filter(item => item.title === 'Gift Card' || item.title === 'Log Out');
 
   return (
     <div className='SideBar'>
@@ -16,27 +26,63 @@ export const Sidebar = () => {
         <PieChartIcon />
       </div>
       <ul className='SideBarList'>
-        {
-          SideBarData.map((val, key) => {
-            return (
-              <li className='SideBarRow' key={key} onClick={(e) => { e.preventDefault(); }}>
-                <div className={`iconTitleContainer ${showTitles ? 'show' : 'hide'}`}>
-                  <div id="icon">{val.icon}</div>
-                  <div id="title">
-                    {val.title === 'Gift Card' ? (
-                      <span className="giftCardMessage">
-                        Free Gift Awaits you!<br />
-                        Upgrade your account &gt;
-                      </span>
-                    ) : (
-                      val.title
-                    )}
-                  </div>
+        {mainItems.map((val, key) => (
+          <li
+            className={`SideBarRow ${selectedItem === key ? 'selected' : ''}`}
+            key={key}
+            onClick={(e) => { 
+              e.preventDefault();
+              handleItemClick(key);
+            }}
+          >
+            <div className={`iconTitleContainer ${showTitles ? 'show' : 'hide'}`}>
+              <div
+                id="icon"
+                className={`icon ${selectedItem === key ? 'selectedIcon' : ''}`}
+              >
+                {val.icon}
+              </div>
+              {showTitles && (
+                <div id="title" className={`title ${selectedItem === key ? 'selectedTitle' : ''}`}>
+                  {val.title}
                 </div>
-              </li>
-            );
-          })
-        }
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <ul className='SideBarList bottom'>
+        {bottomItems.map((val, key) => (
+          <li
+            className={`SideBarRow ${selectedItem === key + mainItems.length ? 'selected' : ''}`}
+            key={key + mainItems.length}
+            onClick={(e) => { 
+              e.preventDefault();
+              handleItemClick(key + mainItems.length);
+            }}
+          >
+            <div className={`iconTitleContainer ${showTitles ? 'show' : 'hide'}`}>
+              <div
+                id="icon"
+                className={`icon ${selectedItem === key + mainItems.length ? 'selectedIcon' : ''}`}
+              >
+                {val.icon}
+              </div>
+              {showTitles && (
+                <div id="title" className={`title ${selectedItem === key + mainItems.length ? 'selectedTitle' : ''}`}>
+                  {val.title === 'Gift Card' ? (
+                    <span className="giftCardMessage">
+                      Free Gift Awaits you!<br />
+                      Upgrade your account &gt;
+                    </span>
+                  ) : (
+                    val.title
+                  )}
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
