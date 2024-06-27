6f9a1d03ff82909cdc5801a32c06023fce8e145c
vaibhav04861 <vaibhavbv1@gmail.com>
2024-06-27 10:34:46 +0530

    sidebar changes

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,21 +1,45 @@
-// src/Sidebar.js
-import React from 'react';
-import './sidebar.css'
-import  {SideBarData} from './SideBarData';
+import React, { useState } from 'react';
+import './sidebar.css';
+import { SideBarData } from './SideBarData';
+import PieChartIcon from '@mui/icons-material/PieChart';
+
 export const Sidebar = () => {
+  const [showTitles, setShowTitles] = useState(false);
+
+  const handleClick = () => {
+    setShowTitles(prevState => !prevState);
+  };
+
   return (
     <div className='SideBar'>
-            <ul className='SideBarList'>
-                {
-                    SideBarData.map((val,key) =>{
-                                return <li className='SideBarRow' key={key} onClick={(e)=>{e.preventDefault()}}>  
-                                {" "}
-                                <div id="icon">{val.icon}</div>{" "}{" "}
-                                <div id="title">{val.title}</div>
-                             </li>;
-                    })
-                }
-            </ul>
+      <div className="randomIcon" onClick={handleClick}>
+        <PieChartIcon />
+      </div>
+      <ul className='SideBarList'>
+        {
+          SideBarData.map((val, key) => {
+            return (
+              <li className='SideBarRow' key={key} onClick={(e) => { e.preventDefault(); }}>
+                <div className={`iconTitleContainer ${showTitles ? 'show' : 'hide'}`}>
+                  <div id="icon">{val.icon}</div>
+                  <div id="title">
+                    {val.title === 'Gift Card' ? (
+                      <span className="giftCardMessage">
+                        Free Gift Awaits you!<br />
+                        Upgrade your account &gt;
+                      </span>
+                    ) : (
+                      val.title
+                    )}
+                  </div>
+                </div>
+              </li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 };
+
+export default Sidebar;
